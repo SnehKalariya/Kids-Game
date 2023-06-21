727abb0f1250afc63fdbb27d389ab0d82f9ce75c
SnehKalariya <snehkalariya9583@gmail.com>
2023-06-21 11:18:47 +0530

    Initial Commit

---
+++ Kids Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kids Game
+//
+//  Created by Sneh kalariya on 21/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
